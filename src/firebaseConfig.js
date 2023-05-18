e8945c744714c2bc9c01df338f8c8ba8e80e3879
yagmursahinn <yagmurrshn@outlook.com>
2023-05-18 11:37:26 +0300

    feat: add cli command

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkOgS6BvUWpjdBAUVR6hlJk_oXg1hX9Tc",
+  authDomain: "linkedinapp-3f128.firebaseapp.com",
+  projectId: "linkedinapp-3f128",
+  storageBucket: "linkedinapp-3f128.appspot.com",
+  messagingSenderId: "225504107021",
+  appId: "1:225504107021:web:940ab990a94498684b1344"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
